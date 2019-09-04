08cbbf70f9eb48c69189d7164f4c05d2a141ea51
Sai Arvind Reddy Desireddy <sd1253@wildcats.unh.edu>
2019-09-04 19:47:08 -0400

    Update App.java

--- ProgrammingAssignment1/src/main/java/App.java
+++ ProgrammingAssignment1/src/main/java/App.java
@@ -1,7 +1,9 @@
 package team07.assign01;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -20,23 +22,20 @@ import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.search.similarities.BasicStats;
-import org.apache.lucene.search.similarities.SimilarityBase;
+import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 import edu.unh.cs.treccar_v2.Data;
 import edu.unh.cs.treccar_v2.read_data.DeserializeData;
 
-
 /*Reading the files from the TREC Complex Answer Retrieval data set that has been unpacked */
 
 
-public class score
-
+public class App
 {
    // private static final String INDEX_DIR = "index";
-   // private static final String FILE_DIR = "D:/test200/test200-train/train.pages.cbor-paragraphs.cbor";
+  //  private static final String FILE_DIR = "D:/test200/test200-train/train.pages.cbor-paragraphs.cbor";
 
     /* Documents are added in the form of Id and Text */
 
@@ -56,24 +55,25 @@ public class score
 
 
     public static void main( String[] args ) throws IOException, ParseException
-    {	
-    	String P = args[1];
-		final String P1 = args[0];
+    { 
+    	System.setProperty("file.encoding", "UTF-8");	
+    	String indexPath = args[1];
+		final String paragraphsFile = args[0];
 
-    	Path path = Paths.get(P);
+        Path path = Paths.get(indexPath);
         Directory Dir = FSDirectory.open(path);
         IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
         IndexWriter writer=  new IndexWriter(Dir, config);
         writer.deleteAll();
-
+        System.out.println("indexing done");
 
         /* Using the DeserializeData.iterableParagraphs,
         each paragraph from the corpus is converted to Lucene document */
 
         /* Using getParaId() and getTextOnly(), paragraph Id and Text is obtained for each para */
 
-        FileInputStream stream = new FileInputStream(new File(P1));
-        for(Data.Paragraph p: DeserializeData.iterableParagraphs(stream)) {
+		FileInputStream stream = new FileInputStream(new File(paragraphsFile));
+		for(Data.Paragraph p: DeserializeData.iterableParagraphs(stream)) {
             String pId = p.getParaId();
             String textOnly = p.getTextOnly();
             Document document = createDocument(pId, textOnly);
@@ -91,7 +91,7 @@ public class score
 
 
 
-        Path path1 = Paths.get(P);
+        Path path1 = Paths.get(indexPath);
         Directory Dir1 = FSDirectory.open(path1);
         IndexReader reader = DirectoryReader.open(Dir1);
 
@@ -99,57 +99,41 @@ public class score
 
         IndexSearcher searcher =new IndexSearcher(reader);
 
+        /* BM25 Retrieval model is used */
 
-
-    /* using the custom Scoriing function, instance of SimilarityBase is created */
-        
-        
-        searcher.setSimilarity(new SimilarityBase() {
-        	@Override
-        	public String toString() {
-        	// TODO Auto-generated method stub
-        	return "similarity base";
-        	}
-
-        	@Override
-        	protected float score(BasicStats stats, float freq, float docLen) {
-        	// TODO Auto-generated method stub
-        	return freq;
-        	}
-        	});
-        
+        searcher.setSimilarity(new BM25Similarity());
 
         int number = 10;
-  
-         String[] entry = new String[3];
-         entry[0]="power nap bene?ts";
-         entry[1]="whale vocalization production of sound ";
-         entry[2]="pokemon puzzle league";
-         System.out.println("Using custom scoring function");
-         
-         for(int i=0;i<entry.length;i++) {
-             System.out.println("Requested query is: "+entry[i]);
 
-             /* QueryParser will convert queries into Lucene queries  */
+        String[] entry = new String[3];
+        entry[0]="power nap benefits";
+        entry[1]="whale vocalization production of sound ";
+        entry[2]="pokemon puzzle league";
+        System.out.println("Using BM25 scoring function");
 
+        for(int i=0;i<entry.length;i++) {
+            System.out.println("Requested query is: "+entry[i]);
 
-             Query  query = new QueryParser("text", new StandardAnalyzer()).parse(entry[i]);
+            /* QueryParser will convert queries into Lucene queries  */
 
 
-             TopDocs top=searcher.search(query,number);
-
-             /* Scoring the top documents using top.scoreDocs  */
-
-             ScoreDoc[] scoreDoc = top.scoreDocs;
-
-             for (ScoreDoc score : scoreDoc) {
-             
-                 final Document doc = searcher.doc(score.doc);
-                 System.out.println(doc.getField("id").stringValue() + "-" + doc.getField("text").stringValue());
-             }System.out.println("------------------------------------------------------------");
-
-         }
+            Query  query = new QueryParser("text", new StandardAnalyzer()).parse(entry[i]);
 
 
-     }
- }
+            TopDocs top=searcher.search(query,number);
+
+            /* Scoring the top documents using top.scoreDocs  */
+
+            ScoreDoc[] scoreDoc = top.scoreDocs;
+
+            for (ScoreDoc score : scoreDoc) {
+            
+                final Document doc = searcher.doc(score.doc);
+                System.out.println(doc.getField("id").stringValue() + "-" + doc.getField("text").stringValue());
+            }System.out.println("------------------------------------------------------------");
+
+        }
+
+
+    }
+}
