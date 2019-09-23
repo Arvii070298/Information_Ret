0476cdac0da76ba8bbd1293fd400c4b4702a991a
Sai Arvind Reddy Desireddy <sd1253@wildcats.unh.edu>
2019-09-23 11:28:24 -0400

    Update Quest3.java

--- ProgrammingAssignment3/Quest3.java
+++ ProgrammingAssignment3/Quest3.java
@@ -77,4 +77,202 @@ public class quest3  {
 	        return out;
 	    }
 	
+	 public static void main(String[] args) throws IOException {
+	    	//final String File=args[0];
+	    	queries = new HashMap<String, String>();
+
+	        File file = new File(File);
+	       
+	        final FileInputStream fileInputStream = new FileInputStream(file);
+	        for(Data.Page p: DeserializeData.iterableAnnotations(fileInputStream)) {
+	            String queryId = p.getPageId();
+	            String query = p.getPageName();
+	            queries.put(queryId, query);
+	    	
+	        }
+	    // final String PARAGRAPH_FILE=args[1];
+	        buildIndex(PARAGRAPH_FILE);
+	        int x=1;
+	        IndexSearcher searcher = setupIndexSearcher();
+	        
+	        if(x==1) {
+	        
+	        SimilarityBase sb= new SimilarityBase() {													//lncltn
+	        	
+                    @Override
+                    protected float score(BasicStats stats, float freq, float docLen)
+                    {
+                    	 float norm = 1;
+                         long docFreq = stats.getDocFreq();
+                         long numDocs = stats.getNumberOfDocuments();
+
+                         double lnc = (1 + Math.log(freq))*1*(1/Math.sqrt(norm));
+                         double ltn = (1 + Math.log(freq))*Math.log(numDocs/(double) docFreq)*1;
+
+                         return (float) (lnc*ltn);
+
+                    }
+
+                    @Override
+                    public String toString()
+                    {
+                        return null;
+                    }
+                 };
+                searcher.setSimilarity(sb);
+			
+	        
+	        
+	       
+	        List<String> content = value(searcher);
+	       
+	        
+	        DEFAULT_SCORE_FILE="lnn.txt";
+	        FileWriter writer = new FileWriter(DEFAULT_SCORE_FILE);
+	        for(String output: content) {
+	            writer.write(output + System.lineSeparator());
+	        }
+	        writer.close();
+	        x++;
+	        }
+	        if(x==2) {																	
+	        SimilarityBase	sb = new SimilarityBase()
+                {																			//bnn-------
+                @Override
+                protected float score(BasicStats stats, float freq, float docLen)
+                {
+                    if(freq > 0)
+                    {
+                        return 1;
+                    }
+                    return 0;
+                }
+
+                @Override
+                public String toString() {
+                    return null;
+                    }
+                };
+                searcher.setSimilarity(sb);
+    			
+    	        
+    	        
+     	       
+    	        List<String> content = value(searcher);
+    	       
+    	        DEFAULT_SCORE_FILE="bnn.txt";
+    	        FileWriter writer = new FileWriter(DEFAULT_SCORE_FILE);
+    	        for(String output: content) {
+    	            writer.write(output + System.lineSeparator());
+    	        }
+    	        writer.close();
+    	        x++;
+	        	
+	        	
+	        	
+	        	
+	        	
+	        }if(x==3) {
+	        	SimilarityBase sb = new SimilarityBase() {						//anc-----------
+                    @Override
+                    protected float score(BasicStats stats, float freq, float docLen) {
+                        double res = (0.5 + ((0.5 * freq)/ stats.getTotalTermFreq()) ) / Math.sqrt(docLen);
+                        return (float) res;
+                    }
+
+                    @Override
+                    public String toString() {
+                        return null;
+                    }
+                };
+                searcher.setSimilarity(sb);
+                List<String> content = value(searcher);
+     	       
+    	        
+    	        DEFAULT_SCORE_FILE="anc.txt";
+    	        FileWriter writer = new FileWriter(DEFAULT_SCORE_FILE);
+    	        for(String output: content) {
+    	            writer.write(output + System.lineSeparator());
+    	        }
+    	        writer.close();            
+                
+	        }
+	     
+	    
+	}
+	    
+	    
+	    
+	    
+	    
+	    public static BooleanQuery toQuery(String queryStr) throws IOException {
+	    	StandardAnalyzer analyzer = new StandardAnalyzer();
+	        ArrayList<String> tokens = new ArrayList<String>();
+	        TokenStream tokenStream = analyzer.tokenStream("text", new StringReader(queryStr));
+	        tokenStream.reset();
+	        tokens.clear();
+	        while (tokenStream.incrementToken()) {
+	            final String token = tokenStream.getAttribute(CharTermAttribute.class).toString();
+	            tokens.add(token);
+	        }
+	        tokenStream.end();
+	        tokenStream.close();
+	        BooleanQuery.Builder booleanQuery = new BooleanQuery.Builder();
+	        for (String token : tokens) {
+	            booleanQuery.add(new TermQuery(new Term("text", token)), BooleanClause.Occur.SHOULD);
+	        }
+	        return booleanQuery.build();
+	    }
+	    public static IndexSearcher setupIndexSearcher() throws IOException {
+	        Path path = FileSystems.getDefault().getPath(INDEX_DIR, "paragraph.lucene");
+	        Directory indexDir = FSDirectory.open(path);
+	        IndexReader reader = DirectoryReader.open(indexDir);
+	        return new IndexSearcher(reader);
+	    }
+
+	   
+	    
+	   
+	    private static IndexWriter createWriter() throws IOException {
+	        Path path = FileSystems.getDefault().getPath(INDEX_DIR, "paragraph.lucene");
+	        Directory indexDir = FSDirectory.open(path);
+	        IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+	        return new IndexWriter(indexDir, config);
+	    }
+	    
+	    public static void buildIndex(String args) throws IOException {
+	    	String PARAGRAPH_FILE=args;
+	    	IndexWriter writer = createWriter();
+	        writer.deleteAll();  // ensure cleaned
+
+	        List<Document> documents = new ArrayList<Document>();
+	        final FileInputStream fileInputStream2 = new FileInputStream(new File(PARAGRAPH_FILE));
+	        for(Data.Paragraph p: DeserializeData.iterableParagraphs(fileInputStream2)) {
+	            String paraId = p.getParaId();
+	            String textOnly = p.getTextOnly();
+	            Document document = new Document();
+	            document.add(new StringField("paragraphid", paraId , Field.Store.YES));
+	            document.add(new TextField("text", textOnly, Field.Store.YES));
+	            documents.add(document);
+	        }
+
+	        writer.addDocuments(documents);
+	        writer.commit();
+	        writer.close();
+	    }
+
+	    
+	    public void writeToFile(String fileName, List<String> content) throws IOException {
+	        FileWriter writer = new FileWriter(fileName);
+	        for(String output: content) {
+	            writer.write(output + System.lineSeparator());
+	        }
+	        writer.close();
+	    }
+	    
+	    
+}
+
+
+
 	
