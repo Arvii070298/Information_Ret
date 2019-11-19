ddcb81969eed12050a268731616b4abf557ddceb
Medhini Shankar Narayan <ms1537@wildcats.unh.edu>
2019-11-19 10:07:37 -0500

    Update Assignment5.java

--- ProgrammingAssignment5/src/main/java/Assignment5.java
+++ ProgrammingAssignment5/src/main/java/Assignment5.java
@@ -16,7 +16,87 @@ import co.nstant.in.cbor.CborException;
 import edu.unh.cs.treccar_v2.Data;
 import edu.unh.cs.treccar_v2.read_data.DeserializeData;
 public class Assignment5 {
-    
+    Assignment4 a4Laplace, a4JMS, a4Dir, a4LncLtn, a4BnnBnn;
+	Assignment3 lnc_ltn, bnn_bnn;
+	//Assignment3 bnn_bnn;
+	
+	ArrayList<Data.Page> pagelist;
+	HashMap<String, ArrayList<String>> relevanceMap = getRelevanceMapFromQrels("output_lm/train.pages.cbor-article.qrels");
+	public static final String RLOUTPUT = "output_lm/ranklib_output";
+	
+	public HashMap<String, ArrayList<String>> getRelevanceMapFromQrels(String qrelsPath){
+		HashMap<String, ArrayList<String>> relMap = new HashMap<String, ArrayList<String>>();
+		BufferedReader br;
+		try{
+			br = new BufferedReader(new FileReader(qrelsPath));
+			String line, paraid, pageid;
+			String[] lineData = new String[4];
+			while((line = br.readLine()) != null){
+				pageid = line.split(" ")[0];
+				paraid = line.split(" ")[2];
+				if(relMap.keySet().contains(pageid)){
+					relMap.get(pageid).add(paraid);
+				}
+				else{
+					ArrayList<String> paralist = new ArrayList<String>();
+					paralist.add(paraid);
+					relMap.put(pageid, paralist);
+				}
+			}
+			br.close();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		return relMap;
+	}
+	
+	public ArrayList<String> getUniqueParaIds(String[] runfiles){
+		ArrayList<String> paraids = new ArrayList<String>();
+		for(int i=0; i<runfiles.length; i++){
+			BufferedReader br;
+			try{
+				br = new BufferedReader(new FileReader(runfiles[i]));
+				String line, paraid;
+				String[] lineData = new String[6];
+				while((line = br.readLine()) != null){
+					paraid = line.split(" ")[2];
+					if(!paraids.contains(paraid))
+						paraids.add(paraid);
+				}
+				br.close();
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+		return paraids;
+	}
+	public void doStuff() throws ParseException{
+		Assignment3 a3 = new Assignment3();
+		a4Laplace = new Assignment4(1);
+		a4JMS = new Assignment4(2);
+		a4Dir = new Assignment4(3);
+		lnc_ltn = new Assignment3();
+		bnn_bnn = new Assignment3();
+		try {
+			a4Laplace.indexAllParas();
+			a4JMS.indexAllParas();
+			a4Dir.indexAllParas();
+			lnc_ltn.indexAllParas(2);
+			bnn_bnn.indexAllParas(3);
+			pagelist = a3.getPageListFromPath(Assignment3.CBOR_OUTLINE);
+			for(Data.Page p:pagelist){
+				a4Laplace.rankParas(p, 10, "a5laplace");
+				a4JMS.rankParas(p, 10, "a5jms");
+				a4Dir.rankParas(p, 10, "a5dir");
+				lnc_ltn.rankParas(p, 10, 2, "output_lm/a5lncltn");
+				bnn_bnn.rankParas(p, 10, 3, "output_lm/a5bnnbnn");
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 public HashMap<String, ArrayList<String>> getRunFileMap(String runfile){
 		HashMap<String, ArrayList<String>> runfileMap = new HashMap<String, ArrayList<String>>();
