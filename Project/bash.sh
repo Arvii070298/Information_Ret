b1447e3076203c42f6b4e0e72bc740e7e800f9ab
Sai Arvind Reddy Desireddy <sd1253@wildcats.unh.edu>
2019-12-03 18:45:18 -0500

    Update bash.sh

--- Project/bash.sh
+++ Project/bash.sh
@@ -23,3 +23,63 @@ rm trec_eval_latest.tar.gz
 mvn clean install
 
 java -jar target/project-1.0-SNAPSHOT-jar-with-dependencies.jar benchmarkY1/benchmarkY1-train/train.pages.cbor-paragraphs.cbor benchmarkY1/benchmarkY1-train/train.pages.cbor-outlines.cbor benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels
+
+cd trec_eval*
+make
+
+echo("------------------------------------------BM25 Results:------------------------------------")
+./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../BM25.txt -m map -m Rprec -m ndcg_cut.20
+
+
+echo("------------------------------------------LNC Results--------------------------------------")
+./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../LNC.txt -m map -m Rprec -m ndcg_cut.20
+
+
+echo("------------------------------------------BNN Results--------------------------------------")
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../BNN.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ echo("------------------------------------------APC Results--------------------------------------")
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../ANC.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ 
+ echo("------------------------------------------LAP Results--------------------------------------")
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../Lap.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ echo("------------------------------------------Jm Results--------------------------------------")
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../Jm.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ echo("------------------------------------------Dir Results--------------------------------------")
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../Dir.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ echo("------------------------------------------bi_word Results--------------------------------------")
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../biword.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ 
+ echo("------------------------------------------Champion List Results--------------------------------------")
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../ChampionList.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ echo("------------------------------------------IndexElimination Results--------------------------------------")
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../IndexElimination.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ echo("------------------------------------------L2R Results--------------------------------------")
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../new_L2R.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
