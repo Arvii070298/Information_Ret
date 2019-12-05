d92166eb672cf967046625b6a11400e990689438
Medhini Shankar Narayan <ms1537@wildcats.unh.edu>
2019-12-05 10:45:04 -0500

    Update bash.sh

--- Project/bash.sh
+++ Project/bash.sh
@@ -76,8 +76,7 @@ echo "------------------------------------------BNN.BNN Results-----------------
 cd ..
 
 
-echo "------------------------------------------L2R Results--------------------------------------"
- java -jar RankLib.jar -train new_L2R.txt -ranker 4 -kcv 5  -save ./benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels -metric2t MAP
+
 
  
 
