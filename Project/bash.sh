dd7b5db6a3694953ce38eaf76373f63a32c49c26
Sai Arvind Reddy Desireddy <sd1253@wildcats.unh.edu>
2019-12-05 09:41:52 -0500

    Update bash.sh

--- Project/bash.sh
+++ Project/bash.sh
@@ -71,11 +71,7 @@ echo "------------------------------------------BNN.BNN Results-----------------
  echo "------------------------------------------IndexElimination Results--------------------------------------"
  ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../IndexElimination.txt -m map -m Rprec -m ndcg_cut.20
  
- cd ..
- echo "------------------------------------------L2R Results--------------------------------------"
- java -jar RankLib.jar -train new_L2R.txt -ranker 4 -kcv 5  -save ./benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels -metric2t MAP
 
- 
  
 
 
