7c7eb7fdb69a32262cb5f6f045d3c2297c1dacd5
Medhini Shankar Narayan <ms1537@wildcats.unh.edu>
2019-12-05 10:44:50 -0500

    Update l2r.sh

---
+++ Project/l2r.sh
@@ -0,0 +1,80 @@
+mkdir Folder1
+cd Folder1
+echo "---------------installing maven-----------"
+
+sudo apt install maven
+
+echo "----------------clone---------------"
+
+git clone https://gitlab.cs.unh.edu/cs753-853-2019/team7.git
+cd team7/Project/
+
+wget http://trec-car.cs.unh.edu/datareleases/v2.0/benchmarkY1-train.v2.0.tar.xz
+
+tar xf benchmarkY1-train.v2.0.tar.xz
+rm xf benchmarkY1-train.v2.0.tar.xz
+
+
+
+wget https://trec.nist.gov/trec_eval/trec_eval_latest.tar.gz
+tar -xf trec_eval_latest.tar.gz
+rm trec_eval_latest.tar.gz
+
+mvn clean install
+
+java -jar target/project-1.0-SNAPSHOT-jar-with-dependencies.jar benchmarkY1/benchmarkY1-train/train.pages.cbor-paragraphs.cbor benchmarkY1/benchmarkY1-train/train.pages.cbor-outlines.cbor benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels
+
+ 
+
+
+cd trec_eval*
+make
+
+echo "------------------------------------------BM25 Results:------------------------------------"
+./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../BM25.txt -m map -m Rprec -m ndcg_cut.20
+
+
+echo "------------------------------------------LNC.LTN Results--------------------------------------"
+./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../LNC.txt -m map -m Rprec -m ndcg_cut.20
+
+
+echo "------------------------------------------BNN.BNN Results--------------------------------------"
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../BNN.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ echo "------------------------------------------ANC.APN Results--------------------------------------"
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../ANC.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ 
+ echo "------------------------------------------UL Results--------------------------------------"
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../Lap.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ echo "------------------------------------------UJM Results--------------------------------------" 
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../Jm.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ echo "------------------------------------------UD Results--------------------------------------"
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../Dir.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ echo "------------------------------------------bi_word Results--------------------------------------" 
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../biword.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ 
+ echo "------------------------------------------Champion List Results--------------------------------------"
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../ChampionList.txt -m map -m Rprec -m ndcg_cut.20
+ 
+ 
+ echo "------------------------------------------IndexElimination Results--------------------------------------"
+ ./trec_eval ../benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels ../IndexElimination.txt -m map -m Rprec -m ndcg_cut.20
+ 
+
+ 
+cd ..
+
+
+echo "------------------------------------------L2R Results--------------------------------------"
+ java -jar RankLib.jar -train new_L2R.txt -ranker 4 -kcv 5  -save ./benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels -metric2t MAP
