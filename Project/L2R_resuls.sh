07d3955c4b8b588c44b010eb3ebf6bf1ff596436
Sai Arvind Reddy Desireddy <sd1253@wildcats.unh.edu>
2019-12-05 09:56:34 -0500

    Update L2R_resuls.sh

--- Project/L2R_resuls.sh
+++ Project/L2R_resuls.sh
@@ -1,4 +1,4 @@
-
+cd Folder/team7/Project/
  echo "------------------------------------------L2R Results--------------------------------------"
  java -jar RankLib.jar -train new_L2R.txt -ranker 4 -kcv 5  -save ./benchmarkY1/benchmarkY1-train/train.pages.cbor-article.qrels -metric2t MAP
 
